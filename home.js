83286960d3100b58c6c4d96c17f004cab67f0cb2
Muhammad Azhan <azhanbhojani56@gmail.com>
2025-03-10 00:02:19 +0500

    Add files via upload

---
+++ home.js
@@ -0,0 +1,14 @@
+$('.banner_slider').slick({
+    dots: true,
+    arrows: true,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 1,
+    autoplay: true,
+    autoplayTimeout: 1000,
+    adaptiveHeight: true,
+    drag: true,
+    prevArrow: '<div class="slick-arrow prev"><span class="fa fa-angle-left"></span></div>',
+    nextArrow: '<div class="slick-arrow next"><span class="fa fa-angle-right"></span></div>'
+  });
+  
